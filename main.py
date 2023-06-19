526f9612376e942bcafe0a1050460e277c9ccf84
EC2 Default User <ec2-user@ip-172-31-71-235.ec2.internal>
2023-06-19 14:52:43 +0000

    fix lint issue

--- main.py
+++ main.py
@@ -1,19 +1,10 @@
 from flask import Flask
-from random import choices
 
 app = Flask(__name__)
 
-
-def random_fruit():
-    """Returns random fruit"""
-    fruits = ["apple", "cherry", "orange"]
-    return choices(fruits)
-
-
 @app.route("/")
 def fruit():
-    """Return random fruit"""
-    my_fruit = random_fruit()
+    """Return Hello messgae"""
     return {"messgae": "Hello CD project-new deploy"}
 
 
